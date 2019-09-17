1cffc3871910edf9fb275ee9bab06930e7addd39
redindian <red_indian@hotmail.com>
2019-09-17 21:06:19 +0200

    Code rewrite.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let Service, Characteristic;
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-gardena", "HomebridgeGardena", MyRobo);
+  homebridge.registerAccessory("homebridge-gardena-lawnmower", "HomebridgeGardena", MyRobo);
 };
 
 function MyRobo(log, config) {
@@ -79,20 +79,20 @@ MyRobo.prototype = {
     return mowerId;
   },
 
-  getDevicesMowerProperties: async function () {
-    const query = 'devices[category=mower].abilities[type=robotic_mower][properties]';
-    return await this.queryDevices(query);
-  },
+  // getDevicesMowerProperties: async function () {
+  //   const query = 'devices[category=mower].abilities[type=robotic_mower][properties]';
+  //   return await this.queryDevices(query);
+  // },
 
   getDevicesMowerStatus: async function () {
     const query = 'devices[category=mower].abilities[type=robotic_mower][properties][name=status].value';
     return await this.queryDevices(query);
   },
 
-  getDevicesBatteryProperties: async function () {
-    const query = 'devices[category=mower].abilities[type=battery_power].properties[name=level].value';
-    return await this.queryDevices(query);
-  },
+  // getDevicesBatteryProperties: async function () {
+  //   const query = 'devices[category=mower].abilities[type=battery_power].properties[name=level].value';
+  //   return await this.queryDevices(query);
+  // },
 
   getDevicesBatteryLevel: async function () {
     const query = 'devices[category=mower].abilities[type=battery_power].properties[name=level].value';
@@ -107,7 +107,7 @@ MyRobo.prototype = {
   queryDevices: async function (query) {
     const data = await this.getDevices();
     const result = jq(query, {data});
-    this.log('queryDevices', {data, query, result});
+    // this.log('queryDevices', {data, query, result});
     return result ? result.value : null;
   },
 
@@ -154,7 +154,7 @@ MyRobo.prototype = {
   getMowerOnCharacteristic: async function (next) {
     const status = await this.getDevicesMowerStatus();
     const mowing = this.isMowingStatus(status) ? 1 : 0;
-    this.log('getMowerOnCharacteristic', {status, mowing});
+    // this.log('getMowerOnCharacteristic', {status, mowing});
     next(null, mowing);
   },
 
@@ -164,20 +164,20 @@ MyRobo.prototype = {
 
   getBatteryLevelCharacteristic: async function (next) {
     const value = await this.getDevicesBatteryLevel();
-    this.log('getBatteryLevelCharacteristic', {value});
+    // this.log('getBatteryLevelCharacteristic', {value});
     next(null, value);
   },
 
   getChargingStateCharacteristic: async function (next) {
     const value = await this.getDevicesBatteryCharging();
-    this.log('getChargingStateCharacteristic', {value});
+    // this.log('getChargingStateCharacteristic', {value});
     next(null, value);
   },
 
   getLowBatteryCharacteristic: async function (next) {
     const value = await this.getDevicesBatteryLevel();
     const low = value < 20;
-    this.log('getLowBatteryCharacteristic', {value, low});
+    // this.log('getLowBatteryCharacteristic', {value, low});
     next(null, low);
   },
 
@@ -263,13 +263,13 @@ MyRobo.prototype = {
     this.services.push(batteryService);
 
     /* Humidity Service */
-/*
-    let humidityService = new Service.HumiditySensor("Battery");
-    humidityService
-      .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-      .on('get', this.getBatteryLevelCharacteristic.bind(this));
-    this.services.push(humidityService);
-*/
+    /*
+        let humidityService = new Service.HumiditySensor("Battery");
+        humidityService
+          .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+          .on('get', this.getBatteryLevelCharacteristic.bind(this));
+        this.services.push(humidityService);
+    */
     /* Switch Service */
 
     /*
@@ -290,207 +290,47 @@ MyRobo.prototype = {
       .on('set', this.setMowerOnCharacteristic.bind(this));
     this.services.push(fanService);
 
-    /* If Robonect HX - fetch temp from temp sensor. Otherwise, fetch battery temp. */
-    /*
-        if (this.card === "HX") {
-          let tempService = new Service.TemperatureSensor("Temperature");
-          tempService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .on('get', this.getTemperatureCharacteristic.bind(this));
-          this.services.push(tempService);
-          this.tempService = tempService;
-        } else {
-          let tempService = new Service.TemperatureSensor("Battery temperature");
-          tempService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .on('get', this.getBatteryTemperatureCharacteristic.bind(this));
-          this.services.push(tempService);
-          this.tempService = tempService;
-        }
-    */
-
-    /*
-    this.informationService = informationService;
-    this.batteryService = batteryService;
-    this.humidityService = humidityService;
-    this.switchService = switchService;
-    this.fanService = fanService;
-     */
-
     return this.services;
   },
   /*
-  getBatteryLevelCharacteristic: function (next) {
-    const me = this;
-    request({
-        url: me.statusUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        return next(null, obj.status.battery);
-      });
-  },
-
-  getChargingStateCharacteristic: function (next) {
-    const me = this;
-    request({
-        url: me.statusUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        var chargingStatus = 0;
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        if (obj.status.status === 4) {
-          chargingStatus = 1;
-        }
-        return next(null, chargingStatus);
-      });
-  },
-
-  getLowBatteryCharacteristic: function (next) {
-    const me = this;
-    request({
-        url: me.statusUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        if (obj.status.battery < 20) {
-          return next(null, 1);
-        } else {
-          return next(null, 0);
-        }
-
-      });
-  },
-
-  getSwitchOnCharacteristic: function (next) {
-    const me = this;
-    var onn = false;
-    request({
-        url: me.statusUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        if (obj.status.mode === 0) {
-          onn = true;
-        }
-        return next(null, onn);
-      });
-  },
-
-  setSwitchOnCharacteristic: function (on, next) {
-    const me = this;
-    if (on) {
-      me.setModeUrl = me.setAutoModeUrl;
-    } else {
-      me.setModeUrl = me.setHomeModeUrl;
-    }
-    request({
-        url: me.setModeUrl,
-        method: 'GET',
-      },
-      function (error, response) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        return next();
-      });
-  },
-
-  getMowerOnCharacteristic: function (next) {
-    const me = this;
-    var mowing = 0;
-    request({
-      url: me.statusUrl,
-      method: 'GET',
+    getSwitchOnCharacteristic: function (next) {
+      const me = this;
+      var onn = false;
+      request({
+          url: me.statusUrl,
+          method: 'GET',
+        },
+        function (error, response, body) {
+          if (error) {
+            me.log(error.message);
+            return next(error);
+          }
+          var obj = JSON.parse(body);
+          if (obj.status.mode === 0) {
+            onn = true;
+          }
+          return next(null, onn);
+        });
     },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        if (obj.status.status === 2 || obj.status.status === 5) {
-          mowing = 1;
-        }
-        return next(null, mowing);
-      });
-  },
 
-  setMowerOnCharacteristic: function (on, next) {
-    const me = this;
-    if (on) {
-      me.setModeUrl = me.setAutoModeUrl;
-    } else {
-      me.setModeUrl = me.setEodModeUrl;
-    }
-    request({
-      url: me.setModeUrl,
-      method: 'GET',
+    setSwitchOnCharacteristic: function (on, next) {
+      const me = this;
+      if (on) {
+        me.setModeUrl = me.setAutoModeUrl;
+      } else {
+        me.setModeUrl = me.setHomeModeUrl;
+      }
+      request({
+          url: me.setModeUrl,
+          method: 'GET',
+        },
+        function (error, response) {
+          if (error) {
+            me.log(error.message);
+            return next(error);
+          }
+          return next();
+        });
     },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        return next();
-      });
-  },
-
-  getTemperatureCharacteristic: function (next) {
-    const me = this;
-    var temperature = 0;
-    request({
-        url: me.tempUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        temperature = obj.health.climate.temperature;
-        return next(null, temperature);
-      });
-  },
-  getBatteryTemperatureCharacteristic: function (next) {
-    const me = this;
-    var temperature = 0;
-    request({
-        url: me.batteryUrl,
-        method: 'GET',
-      },
-      function (error, response, body) {
-        if (error) {
-          me.log(error.message);
-          return next(error);
-        }
-        var obj = JSON.parse(body);
-        temperature = obj.battery.temperature;
-        temperature = temperature / 10;
-        return next(null, temperature);
-      });
-  }
-   */
+     */
 };
